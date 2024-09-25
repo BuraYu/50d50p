18e05fd4bf793126acd270b4e9f850ff2033feb2
BuraYu <burak.yueksel@hotmail.com>
2024-09-25 22:53:35 +0200

    temp home component layout

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
+import "./home.css";
+import proj1 from "../../assets/proj1.png";
 
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
-      <Link to="/pageone">PageOne</Link>
-      <Link to="/pagetwo">PageTwo</Link>
+      <h1 className="home-header">50 Projects</h1>
+      <div className="home-container">
+        <div className="element-container">
+          <Link to="/pageone">
+            <img src={proj1} alt="link to bmi calc" />
+          </Link>
+        </div>
+        <div className="element-container">
+          <Link to="/pageone">PageOne</Link>
+        </div>
+        <div className="element-container">
+          <Link to="/pageone">PageOne</Link>
+        </div>
+        <div className="element-container">
+          <Link to="/pageone">PageOne</Link>
+        </div>
+        <div className="element-container">
+          <Link to="/pageone">PageOne</Link>
+        </div>
+        <div className="element-container">
+          <Link to="/pageone">PageOne</Link>
+        </div>
+      </div>
     </div>
   );
 };
